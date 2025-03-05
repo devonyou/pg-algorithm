8d7aa50c1b5f5a9189ed571ea2d117549ef5f109
devonyou <127083854+devonyou@users.noreply.github.com>
2025-03-05 22:05:29 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,21 @@
 function solution(n) {
-    const dp = Array.from({ length: n + 1 }, () => 1);
+//     const dp = Array.from({ length: n + 1 }, () => 1);
     
-    for(let i=2; i<=n; i++){
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
-    }
+//     for(let i=2; i<=n; i++){
+//         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+//     }
     
-    return dp[n];
+//     return dp[n];
+    
+    let p1 = 1,
+		p2 = 1;
+	let temp = p2;
+
+	for (let i = 2; i <= n; i++) {
+		p2 = (p1 + p2) % 1234567 ;
+		p1 = temp;
+		temp = p2;
+	}
+
+	return p2;
 }
