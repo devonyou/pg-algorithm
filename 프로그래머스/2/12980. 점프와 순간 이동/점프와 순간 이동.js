1152b520161dfd9b60a34b523d79ec120ed8b86a
devonyou <127083854+devonyou@users.noreply.github.com>
2025-03-05 20:52:39 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,13 @@
+function solution(n){
+    let count = 0;
+    
+    while(n > 0){
+        if(n % 2){
+            count++;
+        }
+        
+        n = Math.floor(n / 2);
+    }
+    
+    return count;
+}
