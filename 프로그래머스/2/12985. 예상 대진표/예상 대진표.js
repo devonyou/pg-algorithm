2a2f23843e504e1a3730284707b859a0b6840495
devonyou <127083854+devonyou@users.noreply.github.com>
2025-03-06 18:55:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b){
+    let count = 0;
+    
+    while(a !== b){
+        a = Math.floor(a / 2) + (a % 2);
+        b = Math.floor(b / 2) + (b % 2);
+        
+        count++;
+    }
+    
+    return count;
+}
